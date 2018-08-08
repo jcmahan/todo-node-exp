3c516c300413a13023721e700efc9b3d45de22d7
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-08-07 21:23:31 -0700

    missed one file save, added update and delete

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
-var update = document.getElementById('update')
+var update = document.getElementById('update');
 var del = document.getElementById('delete');
 
-update.addEventListener('click', function() {
+update.addEventListener('click', function () {
     fetch('quotes', {
         method: 'put', 
         headers: {'Content-Type': 'application/json'}, 
